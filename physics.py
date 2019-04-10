a19ef37d38ef0886d81db5322bdb800142311455
MadalinBorodi <49339271+MadalinBorodi@users.noreply.github.com>
2019-04-10 08:50:02 +0300

    Update physics.py

--- physics.py
+++ physics.py
@@ -33,5 +33,5 @@ def get_work(mass,acceleration,distance):
   force = get_force(mass,acceleration)
   return force * distance
 
-train_work=get_work(train_mass,train_acceleration,train_distance
+train_work=get_work(train_mass,train_acceleration,train_distance)
 print ("The GE train does " + str(train_work) + " Joules of work over " + str(train_distance) + " meters.")
